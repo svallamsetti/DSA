18a0dee75cf334ca1cd30c865437a40ea0b77f16
Dinesh Vallamsetti <vallamsettisaidinesh@gmail.com>
2023-10-31 08:14:04 -0600

    Update selection_sort.php

--- selection_sort.php
+++ selection_sort.php
@@ -1,25 +1,25 @@
 <?php
 // Enter your code here, enjoy!
-$array = array(14, 5, 28, 5, 9, 10, 111, 12, 14);
+$array = array(0,114, 5, 28, 5, 9, 10, 111, 12, 14);
 
 function findSmallest($arrayValues){
+	$smallestIndex = 0;
 	$smallest = $arrayValues[0];
 	$lengthOfArray = count($arrayValues);
 	
 	for($i=1; $i< $lengthOfArray; $i++){
-    //flip comparison operator to > to sort descending
 		if($arrayValues[$i] < $smallest){
 			$smallest = $arrayValues[$i];
+			$smallestIndex = $i;
 		}
 	}
-	return $smallest;
+	return $smallestIndex;
 }
 function selection_sort($list){
 	$sortedList = [];
 	while(count($list) > 0){
-		$smallestValue = findSmallest($list);
-		$sortedList[] = $smallestValue;
-		$smallestIndex = array_search($smallestValue, $list);
+		$smallestIndex = findSmallest($list);
+		$sortedList[] = $list[$smallestIndex];
 		array_splice($list, $smallestIndex, 1);
 	}
 	return $sortedList;
